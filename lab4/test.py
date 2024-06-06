df39bcc85e1b7b095417ade3f24f86dcba30168d
YBingQuan <ybq31415926@mail.ustc.edu.cn>
2024-06-06 15:48:44 +0800

    test.py 内容修改(FFT added)

--- lab4/test.py
+++ lab4/test.py
@@ -1,49 +1,92 @@
 import ray
 import time
 import sys
+import math
 import numpy as np
 
 ray.init()
 
-matrix_size: int = 10 # 10 * 10 matrix
-matrix_mul_times: int = 10000000
 if len(sys.argv) < 1:
-    pc_num: int = 10 # default value = 10
+    vector_length: int = 16 # default value = 10
 else:
-    pc_num: int = int(sys.argv[1])
-node_task_num: int = matrix_mul_times // pc_num
+    vector_length: int = int(sys.argv[1])
+    if len(sys.argv[2]) < 2:
+        poly_mul_times: int = 100
+    else:
+        poly_mul_times: int = int(sys.argv[2])
+        if len(sys.argv[2]) < 3:
+            pc_num: int = 10
+        else:
+            pc_num: int = int(sys.argv[3])
+vector_size: int = 2 ** vector_length # 2^16-order polynomial
+
+node_task_num: int = poly_mul_times // pc_num
+
+def FFT(p): # Fast Fourier Transform, p the polyminial
+    n = len(p)
+    if n == 1 :
+        return p
+    w = math.e ** (2 * math.pi * 1j / n)
+    pe = p[::2]
+    po = p[1::2]
+    ye = FFT(pe)
+    yo = FFT(po)
+    y = [0] * n
+    for i in range(n/2):
+        y[i] = ye[i] + w**i * yo[i]
+        y[i + n/2] = ye[i] - w**i * yo[i]
+    return y
+
+def IFFT(p): # Inverse Fast Fourier Transform, p the polyminial
+    n = len(p)
+    if n == 1 :
+        return p
+    w = math.e ** (-2 * math.pi * 1j / n)
+    pe = p[::2]
+    po = p[1::2]
+    ye = IFFT(pe)
+    yo = IFFT(po)
+    y = [0] * n
+    for i in range(n/2):
+        y[i] = ye[i] + w**i * yo[i]
+        y[i + n/2] = ye[i] - w**i * yo[i]
+    return y
 
 
-def matrix_mul(matrix1, matrix2):
-    return np.matmul(matrix1, matrix2)
+
+def polyminial_mul(p1, p2): # coeffs -> FFT, calculate and inverse the result by IFFT
+    coeff1 = FFT(p1)
+    coeff2 = FFT(p2)
+    coeff = [0] * vector_size * 2
+    for i in range (vector_size * 2):
+        coeff[i] = coeff1[i] * coeff2[i]
+    res = IFFT(coeff)
+    for i in range (vector_size * 2):
+        res[i] /= vector_size * 2
+    return res
+
 
 
 @ray.remote
 class Worker(object):
     def __init__(self):
-        self.size = matrix_size
-        self.matrix0 = self.matrix_init2()
-        self.res = self.matrix0.copy()
-        self.all_times = matrix_mul_times
+        self.size = vector_size
+        self.poly = self.poly_init()
+        self.res = self.poly.copy()
+        self.all_times = poly_mul_times
 
-    def matrix_init2(self):
-        # 每行均为正数浮点随机数，单行和为1（归一化）
-        matrix = np.random.random((matrix_size, matrix_size))
-        for row in range(self.size):
-            total = sum(matrix[row])
-            for item in matrix[row]:
-                item /= total
-        # print("Now we have a matrix\n", matrix)
-        return matrix
+    def poly_init(self):
+        vector = np.random.random(size = vector_size)
+        print("Now we have a vector\n", vector)
+        return vector
 
-    # 转移矩阵的极限分布
-    # 计算若干个矩阵的相乘
+    # 计算 p^n
     def calculate(self, times):
         # print("I start doing my work.")
         cur_time = time.time()
-        task_res = self.matrix0.copy()
+        task_res = self.poly.copy()
         for k in range(times-1):
-            task_res = matrix_mul(self.res, self.matrix0)
+            task_res = polyminial_mul(task_res, self.poly)
         # print("I have finished my work, duration: ", time.time() - cur_time)
         return task_res
 
@@ -59,6 +102,6 @@ if __name__ == '__main__':
 
     result = result_list[0]
     for m in result_list:
-        result = matrix_mul(result, m)
+        result = polyminial_mul(result, m)
     # print("final matrix: \n", result)
     print("total duration: ", time.time() - cur_time)

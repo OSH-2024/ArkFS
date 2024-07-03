91db8755a96ed7e31469e630a06269f7f06576e2
Astorathy <1532833931@qq.com>
2024-07-03 10:25:30 +0800

    update search

--- src/LabelMatching/search/main.py
+++ src/LabelMatching/search/main.py
@@ -1,9 +1,9 @@
 from file_search import my_search
 
 if __name__ == "__main__":
-    modified_time_range = ["2024-07-01T00:00:00", "2024-07-01T23:59:59"]
+    modified_time_range = ["2024-07-02T00:00:00", None]
     query_content = "grass pictures"
-    target_folder = "E:\\Codefield\\CODE_C\\Git\\ArkFS\\LabelMatching\\target_folder"
+    target_folder = "E:\\Codefield\\CODE_C\\Git\\ArkFS\\src\\LabelMatching\\target_folder"
 
     image_results, text_results = my_search(modified_time_range, query_content, target_folder)
     print("Image Results:", image_results)

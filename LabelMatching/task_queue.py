bc986887b9887bd5eaf25b5c4bc18ba2f8e9d2e0
YBingQuan <ybq31415926@mail.ustc.edu.cn>
2024-07-04 16:52:57 +0800

    new_folder function added

--- LabelMatching/task_queue.py
+++ LabelMatching/task_queue.py
@@ -44,7 +44,10 @@ class task_queue:
                 src.append(drain)
                 target_folder = self.pop()
                 if len(target_folder) == 0:
-                    src.append(index + "/target_folder")
+                    if len(self.type) == 0:
+                        src.append(index +"/default")
+                    else:
+                        src.append(index + "/target_folder")
                 else:
                     src.append(target_folder)   # <- 演示目录
                 if len(self.type) == 0:
@@ -52,7 +55,10 @@ class task_queue:
                 else:
                     src.append(0)
                 name = np.random.randint(0, 65535)
-                src.append(str(name) + self.type)
+                if len(self.type) == 0:
+                    src.append(str(name))
+                else:
+                    src.append(str(name) +"." +self.type)
                 state = my_add(src) # Error code
                 self.push(drain)
 
@@ -117,7 +123,7 @@ class task_queue:
 def main():
     # print(index)
     # src = [['NULL','NULL'],'txt',[ [index + "/task_queue.py"], ""], "0"]
-    src = [['NULL','NULL'],'txt',[ "ui", ""], "4"]
+    src = [['NULL','NULL'],'',[ [], ""], "0"]
     tqueue = task_queue(src)
     # tqueue.show()
     results = tqueue.execute()
